7be3cdb808aa9f4715996f1f34c04dc0052f1eb8
hrabovskazh <77570308+hrabovskazh@users.noreply.github.com>
2022-03-22 17:40:23 +0100

    function clarification
    just added some comments about the functions

--- mean_median_mode.r
+++ mean_median_mode.r
@@ -15,5 +15,10 @@ getmode <- function(v) {
    uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
+# unique() returns a vector, data frame or array like x but with duplicate elements/rows removed.
+# match() returns a vector of the positions of (first) matches of its first argument in its second.
+#tabulate() takes the integer-valued vector bin and counts the number of times each integer occurs in it.
+#which.max() returns the position of the element with the maximal value in a vector.
+
 #print out mode
 print(round(getmode(X), digits = 1))
